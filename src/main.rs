314065a482f9cdbc9c24f246eec56b0540f17935
Johan-Mi <johanmi@protonmail.com>
2024-05-16 16:30:09 +0200

    Implement CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use anyhow::Result;
+use anyhow::{ensure, Context, Result};
+use std::env;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let mut args = env::args_os().skip(1);
+    ensure!(args.len() <= 1, "too many command line arguments");
+    let project_path = args.next().context("no project file path provided")?;
 
     Ok(())
 }
